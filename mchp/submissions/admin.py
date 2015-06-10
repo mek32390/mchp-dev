1109eea2562a2eba98ae34bb658ce84a79eaf6c9
Andrew Merenbach <andrew@merenbach.com>
2015-06-10 14:27:56 -0700

    Add basic admin for submissions

--- mchp/submissions/admin.py
+++ mchp/submissions/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Syllabus)
+class SyllabusAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Roster)
+class RosterAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.SyllabusReview)
+class SyllabusReviewAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.RosterReview)
+class RosterReviewAdmin(admin.ModelAdmin):
+    pass
